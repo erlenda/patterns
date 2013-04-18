f3ae2bd2573f4100cb5ddb49ca2797f0c8066f2f
Erlend Andresen <erlend.andresen@gmail.com>
2013-04-18 22:14:06 +0200

    added boilerplate code from jquery-boilerplate@github.com

---
+++ js/jquery.boilerplate.options.pattern.js
@@ -0,0 +1,44 @@
+/* 
+	Example from:
+	https://raw.github.com/jquery-boilerplate/patterns/master/patterns/
+		jquery.best.options.plugin-boilerplate.js
+
+	for reference only.
+*/
+
+;(function(defaults, $, window, document, undefined) {
+
+	$.extend({
+		// Function to change the default properties of the plugin
+		// Usage:
+		// jQuery.pluginSetup({property:'Custom value'});
+		pluginSetup : function(options) {
+
+			return $.extend(defaults, options);
+		}
+	}).fn.extend({
+		// Usage:
+		// jQuery(selector).pluginName({property:'value'});
+		pluginName : function(options) {
+
+			options = $.extend({}, defaults, options);
+
+			return $(this).each(function() {
+
+				// Plugin logic
+				// Calling the function:
+				// jQuery(selector).pluginName(options);
+			});
+		},
+
+		otherMethod : function(options) {
+
+			// Some logic
+			// Calling the function:
+			// jQuery(selector).otherMethod(options);
+		}
+	});
+})({
+	property : 'value',
+	otherProperty : 'value'
+}, jQuery, window, document);
